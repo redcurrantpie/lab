0124c1df63619efa3301f3f8121881ec1a2ec5c3
redcurrantpie <polypollianna@gmail.com>
2025-05-09 22:11:43 +0200

    Add files via upload

--- lab1/main.py
+++ lab1/main.py
@@ -1,1 +1,13 @@
+from input_module import input_radiation_every_hour
+from avr_module import calc_average_radiation
+from speed_module import calc_average_speed
 
+radiation_data = input_radiation_every_hour()
+avr = calc_average_radiation(radiation_data)
+speed = calc_average_speed(radiation_data) 
+
+print("Cередньодобовий рівень радіації: ", round(avr, 2))
+if speed >= 0:
+    print("Cередня швидкість підвищення рівня радіації: ", round(speed, 3))
+else:
+    print("Cередня швидкість зниження рівня радіації: ", round(speed, 3))
---
+++ lab1/speed_module.py
@@ -0,0 +1,2 @@
+def calc_average_speed(data):
+    return (data[-1] - data[0])/len(data)
