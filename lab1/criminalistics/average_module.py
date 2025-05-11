3473c383dab94b84be878df236e83fef7c56ffa2
redcurrantpie <polypollianna@gmail.com>
2025-05-11 13:40:29 +0200

    Delete lab1/criminalistics directory

--- lab1/criminalistics/average_module.py
+++
@@ -1,8 +0,0 @@
-from sum_module import calc_sum_row
-
-def calc_print_average(text, categories, years, data):
-    print(f"\n--- Середня кількість злочинів за {text} ---")    
-       
-    for j in range(len(categories)):
-        average = calc_sum_row(data)[j]/len(years)
-        print(f"  {categories[j]:<10} :{average}")
--- lab1/criminalistics/sum_module.py
+++
@@ -1,4 +0,0 @@
-def calc_sum_column(data):
-    return [sum(col) for col in zip(*data)]
-def calc_sum_row(data):
-    return [sum(row) for row in data]
